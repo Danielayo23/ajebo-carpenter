624421beb8f6a34f656dfe25d93cb1b2d11b44ce
CodeKageFX <luqmanola60@gmail.com>
2026-04-07 12:48:27 +0100

    fix: prisma issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -21,9 +21,10 @@ function getMariaDbPoolConfig() {
     user: decodeURIComponent(url.username),
     password: decodeURIComponent(url.password),
     database,
-    connectionLimit: process.env.NODE_ENV === "production"? 10 : 20,
+    connectionLimit: process.env.NODE_ENV === "production" ? 10 : 20,
     // Helps avoid slow hangs on Windows when connection is wrong
     connectTimeout: 10_000,
+    allowPublicKeyRetrieval: true,
   };
 }
 
