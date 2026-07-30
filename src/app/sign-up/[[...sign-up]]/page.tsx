3d0aefa95f41a3f47b37e7b50f36052f47968045
Danielayo23 <141753042+Danielayo23@users.noreply.github.com>
2026-07-31 00:38:45 +0100

    Update page.tsx

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,4 @@
-/*import { SignUp } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 import CustomerHeader from "@/components/customer-header";
 import Footer from "@/components/customer-footer";
 
@@ -69,4 +69,4 @@ export default function Page() {
       <Footer />
     </>
   );
-}*/
+}
